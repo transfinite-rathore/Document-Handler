6dd47aad8a94e7a6498061a8a2dd8e78070d46e8
Swatantra Singh <78965726+transfinite-rathore@users.noreply.github.com>
2023-08-17 12:30:56 +0530

    Update settings.py

--- core/core/settings.py
+++ core/core/settings.py
@@ -127,7 +127,7 @@ USE_TZ = True
 # }
 
 #MEDIA_ROOT =  os.path.join(BASE_DIR, 'public/static') 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = os.path.join(BASE_DIR, 'static'),
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles_build', 'static')
 
